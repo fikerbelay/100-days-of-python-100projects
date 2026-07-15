a719f4e5a0701aa40f2100c922e7415906893270
fikerbelay <fiker23sw@gmail.com>
2026-07-15 15:25:43 +0300

    switched print to print f to handle variables better and created a total amount variable to include bill and tip all together

--- Day 02 Tip Calculator/main.py
+++ Day 02 Tip Calculator/main.py
@@ -3,6 +3,8 @@ print("Welcome to the tip calculator!")
 Bill = float(input("What was the total bill? $"))
 Tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
 Number_of_people = int(input("How many people to split the bill? "))
-Total_Per_person = ((Bill / Tip) + Bill) / Number_of_people
 
-print("Each person should pay: " + " $" + str(Total_Per_person) )
+Total_amount = ((Bill / Tip) + Bill)
+Total_Per_person = round( Total_amount / Number_of_people, 2)
+
+print(f"Each person should pay: ${Total_Per_person}")
