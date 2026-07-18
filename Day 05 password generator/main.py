44239ea88d6205bd7a4aab5aa79aa34acc2665f3
fikerbelay <fiker23sw@gmail.com>
2026-07-18 20:29:56 +0300

    upload

---
+++ Day 05 password generator/main.py
@@ -0,0 +1,27 @@
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password_inputs = []
+for letter in range(nr_letters):
+    password_inputs.append(random.choice(letters))
+for symbole in range(nr_symbols):
+    password_inputs.append(random.choice(symbols))
+for number in range(nr_numbers):
+    password_inputs.append(random.choice(numbers))
+
+password_length = len(password_inputs)
+print(f"ur non mixed up password is {password_inputs}")
+
+
+random.shuffle(password_inputs)
+mixed_password = "".join(password_inputs)
+
+print(f"ur mixed up password is: {mixed_password}")
